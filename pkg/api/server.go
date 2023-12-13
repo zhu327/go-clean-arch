fb2dc72d9986b4e2f5f1349074a1afd80d09dd68
susilnem <susiltiwari750@gmail.com>
2023-12-13 16:44:17 +0545

    add swagger

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	handlerInterface "go-wire/pkg/api/handler/interfaces"
+	"net/http"
 
 	"github.com/gin-gonic/gin"
 	swaggerfiles "github.com/swaggo/files"
@@ -15,6 +16,18 @@ type ServerHTTP struct {
 	Engine *gin.Engine
 }
 
+// @title					Backend API
+// @description				This is a sample server for Backend API.
+//
+// @contact.name				For API Support
+// @contact.email				susiltiwari750@gmail.com
+//
+// @BasePath					/api
+// @SecurityDefinitions.apikey	BearerAuth
+// @Name						Authorization
+// @In							header
+// @Description				Add prefix of Bearer before  token Ex: "Bearer token"
+// @Query.collection.format	multi
 func NewServerHTTP(authHandler handlerInterface.AuthHandler, userHandler handlerInterface.UserHandler) *ServerHTTP {
 	engine := gin.Default()
 
@@ -22,6 +35,12 @@ func NewServerHTTP(authHandler handlerInterface.AuthHandler, userHandler handler
 
 	// swagger docs
 	engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
+
+	engine.NoRoute(func(ctx *gin.Context) {
+		ctx.JSON(http.StatusNotFound, gin.H{
+			"message": "invalid url path",
+		})
+	})
 
 	return &ServerHTTP{
 		Engine: engine,
